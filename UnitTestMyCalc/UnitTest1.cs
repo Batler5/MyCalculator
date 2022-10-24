29bad26d77c0ee89600c203e8f243db5b92b6132
Batler5 <115727657+Batler5@users.noreply.github.com>
2022-10-24 16:05:08 +0300

    MyCalculator
    MyCalculator

---
+++ UnitTestMyCalc/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using WindowsFormsApp1;
+
+namespace UnitTestMyCalc
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod_Sum()
+        {
+            Util util = new Util();
+            int x = 5; int y = 3;
+            int expected = 8;
+            int actual = util.sum(x, y);
+            Assert.AreEqual(expected,actual);
+        }
+        [TestMethod]
+        public void TestMethod_SumX()
+        {
+            Util util = new Util();
+            int x = 3; int y = 3;
+            int expected = 6;
+            int actual = util.sumX(x, y);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod_Operation()
+        {
+            
+            try 
+            {
+                Util util = new Util();
+                util.Operation();
+                Assert.IsTrue(true);
+            }
+            catch 
+            {
+                Assert.IsFalse(false);
+            }
+        }
+    }
+}
